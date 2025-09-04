ac5980106abd719e33cb172efead0fb93c9569b2
dhespinoza <dhespinoza.07@gmail.com>
2025-09-04 08:31:50 -0700

    lab 3.1B is completed

--- lab3_1.cpp
+++ lab3_1.cpp
@@ -14,14 +14,15 @@ using namespace std;
 
 int main()
 {
-    int size = 0;    // The number of values to be averaged
+    int size = 2;    // The number of values to be averaged
     double num1 = 0.0,     // User input number 1
            num2 = 0.0,     // User input number 2
            average = 0.0;  // Average of num1 and num2
 
     // Get the two numbers
     cout << "Enter two numbers separated by one or more spaces: ";
-    cin  >> num1 >> num2;
+    cin  >> num1;
+    cin >> num2;
 
     // Calculate the average
     average = num1 + num2 / size;
